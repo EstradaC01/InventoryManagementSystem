ea52f533a3fdcdd409efe7f8987e493a385c947d
Tyler Jennings <tylerjennings2@outlook.com>
2023-01-14 15:18:22 -0600

    FEATURE-ADMIN-LIST-USERS: Added list users button for admins to view (WIP)

--- app/src/main/java/com/example/inventorymanagementsystem/MainActivity.java
+++ app/src/main/java/com/example/inventorymanagementsystem/MainActivity.java
@@ -1,24 +1,34 @@
 package com.example.inventorymanagementsystem;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     // creating variables for button
-    private Button createProductbtn, itemList;
-
+    private Button createProductbtn, itemList, userList;
     // member fields for logged user
     private String mEmail, mUserid;
+    private boolean isAdmin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +38,10 @@ public class MainActivity extends AppCompatActivity {
         // instantiating firebase firestore database object.
         // firebase firestore database object
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-
         // initializing our buttons
         createProductbtn = findViewById(R.id.idBtnItems);
         itemList = findViewById(R.id.idBtnListItems);
+        userList = findViewById(R.id.idBtnListUsers);
 
         // initializing FirebaseUser
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
@@ -49,6 +59,32 @@ public class MainActivity extends AppCompatActivity {
             // FirebaseUser.getIdToken() instead.
             mUserid = user.getUid();
         }
+        CollectionReference adminRef = db.collection("Users");
+
+        adminRef.whereEqualTo("userId", mUserid).addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                if (!value.isEmpty()) {
+                    List<DocumentSnapshot> list = value.getDocuments();
+                    for (DocumentSnapshot d : list) {
+                        Users u = d.toObject(Users.class);
+                        isAdmin = u.getIsAdmin();
+                    }
+                } else {
+                    Log.d("Error ", "Exception: " + error);
+                }
+                if (!isAdmin) {
+                    userList.setVisibility(View.GONE);
+                } else if (isAdmin) {
+                    userList.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            //Intent i = new Intent(MainActivity.this, );
+                        }
+                    });
+                }
+            }
+        });
 
         // adding onClick listener to view data in new activity
         createProductbtn.setOnClickListener(new View.OnClickListener() {
@@ -58,7 +94,10 @@ public class MainActivity extends AppCompatActivity {
                 Intent i = new Intent(MainActivity.this, AddItem.class);
                 startActivity(i);
             }
-        });
+        }
+
+
+        );
 
         // adding onClick listener to view data in recycler view. view list of items
         itemList.setOnClickListener(new View.OnClickListener() {
