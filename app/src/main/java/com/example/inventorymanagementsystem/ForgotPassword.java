53209dc98868ff50cc9ed1bd2d9480f203a7fe78
Kayle Brown <KLBrown7@student.fullsail.edu>
2023-01-23 20:48:44 -0500

    fixed a few bugs in forgot password

--- app/src/main/java/com/example/inventorymanagementsystem/ForgotPassword.java
+++ app/src/main/java/com/example/inventorymanagementsystem/ForgotPassword.java
@@ -2,18 +2,32 @@ package com.example.inventorymanagementsystem;
 
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.auth.AUTH;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import org.chromium.base.Callback;
 
+import javax.security.auth.callback.Callback;
 
 
 public class ForgotPassword extends AppCompatActivity {
@@ -22,22 +36,7 @@ public class ForgotPassword extends AppCompatActivity {
     private EditText edtEmail;
     private Button btnSubmit;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_forgot_password);
-        viewInitializations();
 
-        //Submit Button
-        btnSubmit = findViewById(R.id.idSubmitbtn);
-        btnSubmit.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                mEmail = edtEmail.getText().toString();
-            }
-
-        });
-    }
 
     void viewInitializations() {
 
@@ -66,5 +65,53 @@ public class ForgotPassword extends AppCompatActivity {
         return Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 
+    public static final FirebaseUser AUTH_USER = FirebaseAuth.getInstance().getCurrentUser()!=null ? FirebaseAuth.getInstance().getCurrentUser(): null ;
 
+    protected final void resetPassword(String emailId,final Callback callback){
+
+        AUTH.sendPasswordResetEmail(emailId)
+
+                .addOnCompleteListener(new OnCompleteListener() {
+
+                    @Override
+
+                    public void onComplete(@NonNull Task task) {
+
+                        if (task.isSuccessful()) {
+
+                            Toast.makeText(getApplicationContext(),"Success",Toast.LENGTH_LONG).show();
+
+                        } else {
+
+                            Toast.makeText(getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_LONG).show();
+
+                        }
+
+                    }
+
+                });
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+        viewInitializations();
+
+        //Submit Button
+        btnSubmit = findViewById(R.id.idSubmitbtn);
+        btnSubmit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                mEmail = edtEmail.getText().toString();
+                if(TextUtils.isEmpty(mEmail))
+                {
+                    edtEmail.setError("Enter valid email address");
+                }
+            }
+
+        });
+    }
 }
