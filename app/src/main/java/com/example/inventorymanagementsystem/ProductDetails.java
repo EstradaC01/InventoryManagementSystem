7e8870df2f8d13bd684aaae573752ec1264843bc
jmonraz <jmonraz@student.fullsail.edu>
2023-01-24 11:06:17 -0800

    Implemented edit button feature on product screen, document on Firebase gets updated.

--- app/src/main/java/com/example/inventorymanagementsystem/ProductDetails.java
+++ app/src/main/java/com/example/inventorymanagementsystem/ProductDetails.java
@@ -8,13 +8,13 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,22 +22,20 @@ import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class ProductDetails extends AppCompatActivity {
 
-    private TextView tvProductId, tvProductDescription;
-    private ImageButton edtChangeProductDetails, edtDeleteProductDetails;
-    private ImageView ivProductImage;
+    private static EditText tvProductId, tvProductDescription;
+    private static ImageButton edtChangeProductDetails, edtDeleteProductDetails;
+    private static Button saveButton;
+    private static ImageView ivProductImage;
     private FirebaseFirestore db;
     private static Products mProduct;
     private static String mCompanyCode;
     private static String mWarehouse;
+
+    private static final String TAG = "ProductDetails";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +46,10 @@ public class ProductDetails extends AppCompatActivity {
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         // change action support bar title and font color
-        getSupportActionBar().setTitle(Html.fromHtml("<font color='#ffffff'>Product Details</font>"));
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(Html.fromHtml("<font color='#ffffff'>Product Details</font>"));
+        }
+
 
         Intent i = getIntent();
         mProduct = (Products)i.getSerializableExtra("Object");
@@ -60,20 +61,42 @@ public class ProductDetails extends AppCompatActivity {
         ivProductImage = findViewById(R.id.productDetailsImageViewProductImage);
         edtChangeProductDetails = findViewById(R.id.productDetailsEditButton);
         edtDeleteProductDetails = findViewById(R.id.productDetailsDeleteButton);
+        saveButton = findViewById(R.id.productDetailsSaveButton);
 
         db = FirebaseFirestore.getInstance();
 
         tvProductId.setText(mProduct.getProductId());
         tvProductDescription.setText(mProduct.getProductDescription());
 
+        tvProductId.setEnabled(false);
+        tvProductDescription.setEnabled(false);
+
         if(mProduct.getImageUri() != null) {
             Glide.with(this).load(mProduct.getImageUri()).into(ivProductImage);
         }
 
-        edtDeleteProductDetails.setOnClickListener(new View.OnClickListener() {
+        edtDeleteProductDetails.setOnClickListener(v -> showCustomDialog());
+
+        edtChangeProductDetails.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showCustomDialog();
+                saveButton.setVisibility(View.VISIBLE);
+                tvProductId.setEnabled(true);
+                tvProductId.requestFocus();
+                tvProductDescription.setEnabled(true);
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CollectionReference d = db.collection(mCompanyCode + "/" + mWarehouse + "/Products");
+
+                d.document(mProduct.getProductId()).delete();
+                // getting data from editText fields.
+                mProduct.setProductId(tvProductId.getText().toString());
+                mProduct.setProductDescription(tvProductDescription.getText().toString());
+                addDataToFireStore(mProduct);
             }
         });
     }
@@ -85,26 +108,34 @@ public class ProductDetails extends AppCompatActivity {
 
         dialog.setContentView(R.layout.dialog_confirmation);
 
-        final TextView title = dialog.findViewById(R.id.dialogConfirmationTitle);
-        final TextView description = dialog.findViewById(R.id.dialogConfirmationDescription);
         Button noButton = dialog.findViewById(R.id.dialogConfirmationNoButton);
         Button yesButton = dialog.findViewById(R.id.dialogConfirmationYesButton);
 
         dialog.show();
-        yesButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CollectionReference d = db.collection(mCompanyCode + "/" + mWarehouse + "/Products");
-                d.document(mProduct.getProductId()).delete();
-                dialog.dismiss();
-                finish();
-            }
+        yesButton.setOnClickListener(v -> {
+            CollectionReference d = db.collection(mCompanyCode + "/" + mWarehouse + "/Products");
+            d.document(mProduct.getProductId()).delete();
+            dialog.dismiss();
+            finish();
         });
 
-        noButton.setOnClickListener(new View.OnClickListener() {
+        noButton.setOnClickListener(v -> dialog.dismiss());
+    }
+
+    private void addDataToFireStore(Products _product) {
+        // creating a collection reference
+        // for our Firebase Firestore database
+
+        db.collection(mCompanyCode + "/"+mWarehouse+"/Products").document(_product.getProductId()).set(_product).addOnSuccessListener(new OnSuccessListener<Void>() {
             @Override
-            public void onClick(View v) {
-                dialog.dismiss();
+            public void onSuccess(Void unused) {
+                Toast.makeText(ProductDetails.this, "Product updated", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(ProductDetails.this, "Fail to add product \n" + e, Toast.LENGTH_LONG).show();
             }
         });
     }
