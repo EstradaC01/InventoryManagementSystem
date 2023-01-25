1f5d041d3675178585a1314ade7dda3a203eb0c3
Kayle Brown <KLBrown7@student.fullsail.edu>
2023-01-24 22:20:01 -0500

    updated issues

--- app/src/main/java/com/example/inventorymanagementsystem/ForgotPassword.java
+++ app/src/main/java/com/example/inventorymanagementsystem/ForgotPassword.java
@@ -15,12 +15,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.crashlytics.buildtools.reloc.org.apache.http.auth.AUTH;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 import org.chromium.base.Callback;
 
