6ed4198e57c637d65f6a7b9a87605a9caa08be3a
Kayle Brown <KLBrown7@student.fullsail.edu>
2023-01-24 22:18:23 -0500

    updated issues

--- app/src/main/java/com/example/inventorymanagementsystem/ForgotPassword.java
+++ app/src/main/java/com/example/inventorymanagementsystem/ForgotPassword.java
@@ -21,13 +21,9 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.chromium.base.Callback;
 
-import javax.security.auth.callback.Callback;
 
 
 public class ForgotPassword extends AppCompatActivity {
@@ -37,60 +33,12 @@ public class ForgotPassword extends AppCompatActivity {
     private Button btnSubmit;
 
 
-
     void viewInitializations() {
 
         edtEmail = findViewById(R.id.idEdtForgotPasswordUI);
         mEmail = edtEmail.getText().toString();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    }
-
-    // Checking if the input in form is valid
-    boolean validateInput() {
-
-        if (mEmail.equals("")) {
-            edtEmail.setError("Please Enter Email");
-            return false;
-        }
-        // checking the proper email format
-        if (!isEmailValid(mEmail)) {
-            edtEmail.setError("Please Enter Valid Email");
-            return false;
-        }
-        return true;
-    }
-
-    boolean isEmailValid(String email) {
-        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
-    }
-
-    public static final FirebaseUser AUTH_USER = FirebaseAuth.getInstance().getCurrentUser()!=null ? FirebaseAuth.getInstance().getCurrentUser(): null ;
-
-    protected final void resetPassword(String emailId,final Callback callback){
-
-        AUTH.sendPasswordResetEmail(emailId)
-
-                .addOnCompleteListener(new OnCompleteListener() {
-
-                    @Override
-
-                    public void onComplete(@NonNull Task task) {
-
-                        if (task.isSuccessful()) {
-
-                            Toast.makeText(getApplicationContext(),"Success",Toast.LENGTH_LONG).show();
-
-                        } else {
-
-                            Toast.makeText(getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_LONG).show();
-
-                        }
-
-                    }
-
-                });
-
     }
 
     @Override
@@ -114,4 +62,52 @@ public class ForgotPassword extends AppCompatActivity {
 
         });
     }
+
+    // Checking if the input in form is valid
+    boolean validateInput() {
+
+        if (mEmail.equals("")) {
+            edtEmail.setError("Please Enter Email");
+            return false;
+        }
+        // checking the proper email format
+        if (!isEmailValid(mEmail)) {
+            edtEmail.setError("Please Enter Valid Email");
+            return false;
+        }
+        return true;
+    }
+
+    boolean isEmailValid(String email) {
+
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    protected final void resetPassword(String emailId,final Callback callback){
+
+        FirebaseAuth.getInstance().sendPasswordResetEmail(emailId)
+
+                .addOnCompleteListener(new OnCompleteListener() {
+
+                    @Override
+
+                    public void onComplete(@NonNull Task task) {
+
+                        if (task.isSuccessful()) {
+
+                            Toast.makeText(getApplicationContext(),"Success",Toast.LENGTH_LONG).show();
+
+                        } else {
+
+                            Toast.makeText(getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_LONG).show();
+
+                        }
+
+                    }
+
+                });
+
+    }
+
+
 }
