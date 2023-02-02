9b9012c1b850f03aa1e72b29980f99b934da9034
jorgemonraz <jmonraz@student.fullsail.edu>
2023-02-01 18:21:20 -0800

    JAVA-CLASS-WORK: Initialized widgets from ReceivingScreen layout in ReceivingScreen.java

--- app/src/main/java/com/example/inventorymanagementsystem/ReceivingScreen.java
+++ app/src/main/java/com/example/inventorymanagementsystem/ReceivingScreen.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.text.Html;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class ReceivingScreen extends AppCompatActivity {
 
@@ -18,5 +21,22 @@ public class ReceivingScreen extends AppCompatActivity {
 
         // change action support bar title and font color
         getSupportActionBar().setTitle(Html.fromHtml("<font color='#ffffff'>Receiving</font>"));
+
+        // Initialize widgets
+        EditText edtProductId = findViewById(R.id.edtReceivingScreenProductId);
+        EditText edtNumberOfUnits = findViewById(R.id.edtReceivingScreenNumberOfUnits);
+        EditText edtPiecesPerBox = findViewById(R.id.edtReceivingScreenPiecesPerBox);
+        EditText edtNumberOfBoxes = findViewById(R.id.edtReceivingScreenTotalBoxes);
+        EditText edtLoosePieces = findViewById(R.id.edtReceivingScreenLoosePieces);
+        Spinner spinnerDisposition = findViewById(R.id.spinnerReceivingScreenDisposition);
+        EditText edtLocation = findViewById(R.id.edtReceivingScreenLocation);
+        EditText edtWeight = findViewById(R.id.edtReceivingScreenWeight);
+        EditText edtPO = findViewById(R.id.edtReceivingScreenPO);
+        EditText edtShipFrom = findViewById(R.id.edtReceivingScreenShipFrom);
+
+        Button btnSubmit = findViewById(R.id.btnReceivingScreenSubmit);
+        Button btnAddProduct = findViewById(R.id.btnReceivingScreenAddProduct);
+        Button btnAddPO = findViewById(R.id.btnReceivingScreenAddPO);
+        Button btnAddLocaiton = findViewById(R.id.btnReceivingScreenAddLocation);
     }
 }
