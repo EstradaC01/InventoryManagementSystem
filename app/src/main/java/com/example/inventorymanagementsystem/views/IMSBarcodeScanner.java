150b2158e3595b4b17a10bfe5d7f55a7d22a222f
Tyler Jennings <tylerjennings2@outlook.com>
2023-02-07 13:06:24 -0600

    UPDATE-SCANNER: Updated scanner layout and options

--- app/src/main/java/com/example/inventorymanagementsystem/views/IMSBarcodeScanner.java
+++ app/src/main/java/com/example/inventorymanagementsystem/views/IMSBarcodeScanner.java
@@ -1,20 +1,54 @@
 package com.example.inventorymanagementsystem.views;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.inventorymanagementsystem.R;
+import com.example.inventorymanagementsystem.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.common.util.concurrent.ListenableFuture;
 import com.google.mlkit.vision.barcode.BarcodeScanner;
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions;
 import com.google.mlkit.vision.barcode.BarcodeScanning;
 import com.google.mlkit.vision.barcode.common.Barcode;
+import com.google.mlkit.vision.common.InputImage;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class IMSBarcodeScanner extends AppCompatActivity {
 
@@ -22,6 +56,8 @@ public class IMSBarcodeScanner extends AppCompatActivity {
     public static final int REQUEST_CODE_PERMISSIONS = 10;
     public static final String TAG = "CameraXApp";
     private ExecutorService cameraExecutor;
+    private ImageCapture imageCapture = null;
+
     private static final String[] REQUIRED_PERMISSIONS;
 
     static {
@@ -31,13 +67,16 @@ public class IMSBarcodeScanner extends AppCompatActivity {
         }
         REQUIRED_PERMISSIONS = requiredPermissions.toArray(new String[0]);
     }
+    private ActivityMainBinding viewBinding;
+    private String fileName;
     private BarcodeScannerOptions barcodeOptions;
     private BarcodeScanner scanner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_barcode_scanner);
+        viewBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(viewBinding.getRoot());
         barcodeOptions =
                 new BarcodeScannerOptions.Builder()
                         .setBarcodeFormats(
@@ -46,5 +85,174 @@ public class IMSBarcodeScanner extends AppCompatActivity {
                         .build();
 
         scanner = BarcodeScanning.getClient(barcodeOptions);
+
+
+    }
+    private void scanBarcodes(InputImage image) {
+        // [START set_detector_options]
+        BarcodeScannerOptions options =
+                new BarcodeScannerOptions.Builder()
+                        .setBarcodeFormats(
+                                Barcode.FORMAT_QR_CODE,
+                                Barcode.FORMAT_AZTEC)
+                        .build();
+        // [END set_detector_options]
+
+        // [START get_detector]
+        BarcodeScanner scanner = BarcodeScanning.getClient();
+        // Or, to specify the formats to recognize:
+        // BarcodeScanner scanner = BarcodeScanning.getClient(options);
+        // [END get_detector]
+
+        // [START run_detector]
+        Task<List<Barcode>> result = scanner.process(image)
+                .addOnSuccessListener(new OnSuccessListener<List<Barcode>>() {
+                    @Override
+                    public void onSuccess(List<Barcode> barcodes) {
+                        // Task completed successfully
+                        // [START_EXCLUDE]
+                        // [START get_barcodes]
+                        for (Barcode barcode: barcodes) {
+                            Rect bounds = barcode.getBoundingBox();
+                            Point[] corners = barcode.getCornerPoints();
+
+                            String rawValue = barcode.getRawValue();
+
+                            int valueType = barcode.getValueType();
+                            // See API reference for complete list of supported types
+                            switch (valueType) {
+                                case Barcode.TYPE_WIFI:
+                                    String ssid = barcode.getWifi().getSsid();
+                                    String password = barcode.getWifi().getPassword();
+                                    int type = barcode.getWifi().getEncryptionType();
+                                    break;
+                                case Barcode.TYPE_URL:
+                                    String title = barcode.getUrl().getTitle();
+                                    String url = barcode.getUrl().getUrl();
+                                    break;
+                            }
+                        }
+                        // [END get_barcodes]
+                        // [END_EXCLUDE]
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Task failed with an exception
+                        // ...
+                    }
+                });
+        // [END run_detector]
+    }
+    private void takePhoto() {
+        ImageCapture imageCapture = this.imageCapture;
+        if (imageCapture == null) return;
+
+        SimpleDateFormat nameFormat = new SimpleDateFormat(FILENAME_FORMAT, Locale.US);
+        fileName = nameFormat.format(System.currentTimeMillis());
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, fileName);
+        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+            contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image");
+        }
+
+        ImageCapture.OutputFileOptions outputOptions = new ImageCapture.OutputFileOptions.Builder(
+                getContentResolver(),
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                contentValues
+        ).build();
+        imageCapture.takePicture(Executors.newSingleThreadExecutor(), new ImageCapture.OnImageCapturedCallback() {
+            @Override
+            @androidx.camera.core.ExperimentalGetImage
+            public void onCaptureSuccess(@NonNull ImageProxy image) {
+                super.onCaptureSuccess(image);
+                image.getImage();
+                image.getImageInfo().getRotationDegrees();
+
+            }
+            @Override
+            public void onError(@NonNull ImageCaptureException exception) {
+                super.onError(exception);
+            }
+        });
+        imageCapture.takePicture(
+                outputOptions,
+                Executors.newSingleThreadExecutor(),
+                new ImageCapture.OnImageSavedCallback() {
+                    @Override
+                    public void onError(@NonNull ImageCaptureException exception) {
+                        Log.e(TAG, "Photo capture failed: " + exception.getMessage(), exception);
+                    }
+                    @Override
+                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                        String msg = "Photo capture succeeded: " + outputFileResults.getSavedUri();
+                        Log.d(TAG, msg);
+                    }
+                });
+    }
+    private void startCamera() {
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+
+                CameraSelector cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA;
+
+                Preview preview = new Preview.Builder().build();
+                try {
+                    cameraProvider.unbindAll();
+                    cameraProvider.bindToLifecycle(IMSBarcodeScanner.this, cameraSelector, preview,imageCapture);
+                } catch (Exception exception) {
+                    Log.e(TAG, "Use case binding failed", exception);
+                }
+            } catch (ExecutionException | InterruptedException e) {
+                Log.e(TAG, "CameraProviderFuture exception", e);
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cameraExecutor.shutdown();
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera();
+            } else {
+                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    private boolean allPermissionsGranted() {
+        for (String permission : REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private class YourAnalyzer implements ImageAnalysis.Analyzer {
+        public YourAnalyzer() {
+        }
+
+        @Override
+        @androidx.camera.core.ExperimentalGetImage
+        public void analyze(ImageProxy imageProxy) {
+            Image mediaImage = imageProxy.getImage();
+            if (mediaImage != null) {
+                InputImage image =
+                        InputImage.fromMediaImage(mediaImage, imageProxy.getImageInfo().getRotationDegrees());
+
+            }
+        }
     }
 }
