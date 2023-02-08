551c392a0e6a4ae7bba850ddfc83aba869472ae2
Tyler Jennings <tylerjennings2@outlook.com>
2023-02-08 06:04:42 -0600

    FEATURE-BARCODE-SCANNER: Implemented barcode scanner

--- app/src/main/java/com/example/inventorymanagementsystem/views/IMSBarcodeScanner.java
+++ app/src/main/java/com/example/inventorymanagementsystem/views/IMSBarcodeScanner.java
@@ -3,34 +3,27 @@ package com.example.inventorymanagementsystem.views;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraSelector;
-import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageCapture;
 import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.core.content.ContextCompat;
-import androidx.fragment.app.FragmentManager;
-
-import android.content.ContentUris;
-import android.content.ContentValues;
-import android.content.Context;
 import android.content.pm.PackageManager;
-import android.database.Cursor;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.media.Image;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.DocumentsContract;
-import android.provider.MediaStore;
 import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.inventorymanagementsystem.R;
-import com.example.inventorymanagementsystem.databinding.ActivityMainBinding;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
@@ -41,11 +34,9 @@ import com.google.mlkit.vision.barcode.BarcodeScanning;
 import com.google.mlkit.vision.barcode.common.Barcode;
 import com.google.mlkit.vision.common.InputImage;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -67,16 +58,16 @@ public class IMSBarcodeScanner extends AppCompatActivity {
         }
         REQUIRED_PERMISSIONS = requiredPermissions.toArray(new String[0]);
     }
-    private ActivityMainBinding viewBinding;
-    private String fileName;
+    private SurfaceView cameraPreview;
     private BarcodeScannerOptions barcodeOptions;
     private BarcodeScanner scanner;
+    private Button scanBtn;
+    private TextView tvBarcode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        viewBinding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(viewBinding.getRoot());
+        setContentView(R.layout.activity_barcode_scanner);
         barcodeOptions =
                 new BarcodeScannerOptions.Builder()
                         .setBarcodeFormats(
@@ -85,24 +76,34 @@ public class IMSBarcodeScanner extends AppCompatActivity {
                         .build();
 
         scanner = BarcodeScanning.getClient(barcodeOptions);
+        scanBtn = findViewById(R.id.idBtnScanCode);
+        tvBarcode = findViewById(R.id.idTVBarcode);
 
+        cameraExecutor = Executors.newSingleThreadExecutor();
 
+        imageCapture = new ImageCapture.Builder().build();
+
+        cameraPreview = findViewById(R.id.cameraView);
+        cameraPreview.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                startCamera();
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) { }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) { }
+        });
+        scanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePhoto();
+            }
+        });
     }
     private void scanBarcodes(InputImage image) {
-        // [START set_detector_options]
-        BarcodeScannerOptions options =
-                new BarcodeScannerOptions.Builder()
-                        .setBarcodeFormats(
-                                Barcode.FORMAT_QR_CODE,
-                                Barcode.FORMAT_AZTEC)
-                        .build();
-        // [END set_detector_options]
-
-        // [START get_detector]
-        BarcodeScanner scanner = BarcodeScanning.getClient();
-        // Or, to specify the formats to recognize:
-        // BarcodeScanner scanner = BarcodeScanning.getClient(options);
-        // [END get_detector]
 
         // [START run_detector]
         Task<List<Barcode>> result = scanner.process(image)
@@ -113,24 +114,9 @@ public class IMSBarcodeScanner extends AppCompatActivity {
                         // [START_EXCLUDE]
                         // [START get_barcodes]
                         for (Barcode barcode: barcodes) {
-                            Rect bounds = barcode.getBoundingBox();
-                            Point[] corners = barcode.getCornerPoints();
-
                             String rawValue = barcode.getRawValue();
-
-                            int valueType = barcode.getValueType();
-                            // See API reference for complete list of supported types
-                            switch (valueType) {
-                                case Barcode.TYPE_WIFI:
-                                    String ssid = barcode.getWifi().getSsid();
-                                    String password = barcode.getWifi().getPassword();
-                                    int type = barcode.getWifi().getEncryptionType();
-                                    break;
-                                case Barcode.TYPE_URL:
-                                    String title = barcode.getUrl().getTitle();
-                                    String url = barcode.getUrl().getUrl();
-                                    break;
-                            }
+                            tvBarcode.setText(rawValue);
+                            barcodes.clear();
                         }
                         // [END get_barcodes]
                         // [END_EXCLUDE]
@@ -148,49 +134,23 @@ public class IMSBarcodeScanner extends AppCompatActivity {
     private void takePhoto() {
         ImageCapture imageCapture = this.imageCapture;
         if (imageCapture == null) return;
-
-        SimpleDateFormat nameFormat = new SimpleDateFormat(FILENAME_FORMAT, Locale.US);
-        fileName = nameFormat.format(System.currentTimeMillis());
-        ContentValues contentValues = new ContentValues();
-        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, fileName);
-        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
-            contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image");
-        }
-
-        ImageCapture.OutputFileOptions outputOptions = new ImageCapture.OutputFileOptions.Builder(
-                getContentResolver(),
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                contentValues
-        ).build();
         imageCapture.takePicture(Executors.newSingleThreadExecutor(), new ImageCapture.OnImageCapturedCallback() {
             @Override
             @androidx.camera.core.ExperimentalGetImage
             public void onCaptureSuccess(@NonNull ImageProxy image) {
                 super.onCaptureSuccess(image);
-                image.getImage();
-                image.getImageInfo().getRotationDegrees();
-
+                Image mediaImage = image.getImage();
+                if (mediaImage != null) {
+                    InputImage inputImage =
+                            InputImage.fromMediaImage(mediaImage, image.getImageInfo().getRotationDegrees());
+                    scanBarcodes(inputImage);
+                }
             }
             @Override
             public void onError(@NonNull ImageCaptureException exception) {
                 super.onError(exception);
             }
         });
-        imageCapture.takePicture(
-                outputOptions,
-                Executors.newSingleThreadExecutor(),
-                new ImageCapture.OnImageSavedCallback() {
-                    @Override
-                    public void onError(@NonNull ImageCaptureException exception) {
-                        Log.e(TAG, "Photo capture failed: " + exception.getMessage(), exception);
-                    }
-                    @Override
-                    public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
-                        String msg = "Photo capture succeeded: " + outputFileResults.getSavedUri();
-                        Log.d(TAG, msg);
-                    }
-                });
     }
     private void startCamera() {
         ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
@@ -201,6 +161,7 @@ public class IMSBarcodeScanner extends AppCompatActivity {
                 CameraSelector cameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA;
 
                 Preview preview = new Preview.Builder().build();
+                preview.setSurfaceProvider((Preview.SurfaceProvider) cameraPreview);
                 try {
                     cameraProvider.unbindAll();
                     cameraProvider.bindToLifecycle(IMSBarcodeScanner.this, cameraSelector, preview,imageCapture);
@@ -238,21 +199,5 @@ public class IMSBarcodeScanner extends AppCompatActivity {
             }
         }
         return true;
-    }
-
-    private class YourAnalyzer implements ImageAnalysis.Analyzer {
-        public YourAnalyzer() {
-        }
-
-        @Override
-        @androidx.camera.core.ExperimentalGetImage
-        public void analyze(ImageProxy imageProxy) {
-            Image mediaImage = imageProxy.getImage();
-            if (mediaImage != null) {
-                InputImage image =
-                        InputImage.fromMediaImage(mediaImage, imageProxy.getImageInfo().getRotationDegrees());
-
-            }
-        }
     }
 }
