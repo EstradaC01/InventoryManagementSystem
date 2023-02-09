43a422e4cd0244f5d89418ed441ad5d37304af20
jorgemonraz <jmonraz@student.fullsail.edu>
2023-02-09 11:47:27 -0800

    ADD-LAYOUT: Added layout for unit adjustment within inventory sub menu.

---
+++ app/src/main/java/com/example/inventorymanagementsystem/views/UnitAdjustment.java
@@ -0,0 +1,16 @@
+package com.example.inventorymanagementsystem.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.inventorymanagementsystem.R;
+
+public class UnitAdjustment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unit_adjustment);
+    }
+}
