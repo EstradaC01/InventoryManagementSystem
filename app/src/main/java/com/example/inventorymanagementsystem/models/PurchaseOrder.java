b2ee7793e653afa108123b4890e5e2766f9c913b
jorgemonraz <jmonraz@student.fullsail.edu>
2023-02-10 15:25:29 -0800

    ADD-LAYOUT: Added layout for purchase order product card.

--- app/src/main/java/com/example/inventorymanagementsystem/models/PurchaseOrder.java
+++ app/src/main/java/com/example/inventorymanagementsystem/models/PurchaseOrder.java
@@ -1,4 +1,68 @@
 package com.example.inventorymanagementsystem.models;
 
+import java.util.ArrayList;
+
 public class PurchaseOrder {
+    ArrayList<Products> products;
+    String anticipatedArrivalDate, poNumber, shippingFrom, dateCreated, status;
+
+    public PurchaseOrder() {
+
+    }
+    public PurchaseOrder(ArrayList<Products> products, String anticipatedArrivalDate, String poNumber, String shippingFrom, String dateCreated, String status) {
+        this.products = products;
+        this.anticipatedArrivalDate = anticipatedArrivalDate;
+        this.poNumber = poNumber;
+        this.shippingFrom = shippingFrom;
+        this.dateCreated = dateCreated;
+        this.status = status;
+    }
+
+    public ArrayList<Products> getProducts() {
+        return products;
+    }
+
+    public void setProducts(ArrayList<Products> products) {
+        this.products = products;
+    }
+
+    public String getAnticipatedArrivalDate() {
+        return anticipatedArrivalDate;
+    }
+
+    public void setAnticipatedArrivalDate(String anticipatedArrivalDate) {
+        this.anticipatedArrivalDate = anticipatedArrivalDate;
+    }
+
+    public String getPoNumber() {
+        return poNumber;
+    }
+
+    public void setPoNumber(String poNumber) {
+        this.poNumber = poNumber;
+    }
+
+    public String getShippingFrom() {
+        return shippingFrom;
+    }
+
+    public void setShippingFrom(String shippingFrom) {
+        this.shippingFrom = shippingFrom;
+    }
+
+    public String getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(String dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
--- app/src/main/java/com/example/inventorymanagementsystem/views/AddPurchaseOrder.java
+++ app/src/main/java/com/example/inventorymanagementsystem/views/AddPurchaseOrder.java
@@ -1,16 +1,76 @@
 package com.example.inventorymanagementsystem.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.inventorymanagementsystem.R;
+import com.example.inventorymanagementsystem.models.Products;
+import com.example.inventorymanagementsystem.models.Users;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
 
 public class AddPurchaseOrder extends AppCompatActivity {
+
+    FirebaseFirestore db;
+    private Users mCurrentUser;
+    private String mWarehouse;
+    private RecyclerView mRecyclerView;
+    private EditText edtAnticipatedArrivalDate, edtPoNumber, edtShippingFrom;
+
+    String mAnticipatedArrivalDate, mPoNumber, mShippingFrom;
+
+    ArrayList<Products> mProductsArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_purchase_order);
+
+        // prevents users from rotating screen
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        // change action support bar title and font color
+        getSupportActionBar().setTitle(Html.fromHtml("<font color='#ffffff'>Purchase Order</font>"));
+
+        // initialize firebase
+        db = FirebaseFirestore.getInstance();
+
+        Button btnSubmit = findViewById(R.id.addPurchaseOrderBtnSubmit);
+        Button btnAddRemoveProduct = findViewById(R.id.addPurchaseOrderBtnAddRemoveProducts);
+        edtAnticipatedArrivalDate = findViewById(R.id.addPurchaseOrderEdtAnticipatedArrivalDate);
+        edtPoNumber = findViewById(R.id.addPurchaseOrderEdtPONumber);
+        edtShippingFrom = findViewById(R.id.addPurchaseOrderEdtShippingFrom);
+        mRecyclerView = findViewById(R.id.addPurchaseOrderRecyclerView);
+
+
+        Intent i = getIntent();
+        mCurrentUser = (Users)i.getSerializableExtra("User");
+        mWarehouse = (String) i.getSerializableExtra("Warehouse");
+
+        btnSubmit.setOnClickListener(v -> {
+
+            if(TextUtils.isEmpty(mAnticipatedArrivalDate)) {
+                edtAnticipatedArrivalDate.setError("Enter Anticipated Arrival Date");
+            }
+            if(TextUtils.isEmpty(mPoNumber)) {
+                edtPoNumber.setError("Enter PO Number");
+            }
+            if(TextUtils.isEmpty(mShippingFrom)) {
+                edtShippingFrom.setError("Enter Shipping From");
+            }
+        });
+
+        btnAddRemoveProduct.setOnClickListener(v -> {
+
+        });
     }
 }
